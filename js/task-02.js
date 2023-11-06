e04a65c94211f5d64e580e16a1ae87f0ee9fd52a
TikeyFTW <tikey91@gmail.com>
2023-11-06 13:40:58 +0100

    final

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulElement = document.querySelector("#ingredients");
+
+ingredients.forEach(function (ingredient) {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+
+  ulElement.appendChild(liElement);
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const formData = {};
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wszystkie pola powinny zostać uzupełnione!");
+  } else {
+    formData.email = email.value;
+    formData.password = password.value;
+
+    e.currentTarget.reset();
+
+    console.log(formData);
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
